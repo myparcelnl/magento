a7cc4794b725a66c91acfb9446831563d53e1cb5
Richard Perdaan <richardperdaan@live.nl>
2017-04-21 15:33:18 +0200

    Text on the support tab
    1. Add text on the page + version number
    2. Add GitHub issues

--- Block/System/Config/Form/SupportTabRepository.php
+++ Block/System/Config/Form/SupportTabRepository.php
@@ -20,10 +20,57 @@ namespace MyParcelNL\Magento\Block\System\Config\Form;
 
 class SupportTabRepository extends \Magento\Sales\Block\Adminhtml\Order\AbstractOrder
 {
+    const MODULE_NAME = 'MyParcelNL_Magento';
+
+    /**
+     * @var \Magento\Framework\Module\ModuleListInterface
+     */
+    protected $moduleList;
+
+    /**
+     * @todo add description
+     *
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Sales\Helper\Admin $adminHelper
+     * @param \Magento\Framework\Module\ModuleListInterface $moduleList
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Sales\Helper\Admin $adminHelper,
+        \Magento\Framework\Module\ModuleListInterface $moduleList,
+        array $data = []
+    ) {
+        $this->moduleList = $moduleList;
+
+        parent::__construct($context, $registry, $adminHelper, $data);
+        $this->moduleList = $moduleList;
+    }
+
+    /**
+     * @todo add description
+     *
+     * @return string
+     */
     public function getCssUrl()
     {
         $cssUrl = $this->_assetRepo->createAsset('MyParcelNL_Magento::css/config/support_tab/style.css')->getUrl();
 
         return $cssUrl;
+    }
+
+    /**
+     * @todo add description
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        $moduleCode = self::MODULE_NAME;
+        $moduleInfo = $this->moduleList->getOne($moduleCode);
+
+        return (string)$moduleInfo['setup_version'];
     }
 }
--- view/adminhtml/web/js/config/support_tab/projects.js
+++ view/adminhtml/web/js/config/support_tab/projects.js
@@ -1,1 +1,78 @@
-alert('testes');
+/**
+ *
+ * Get issues from github
+ * Trigger actions:
+ * - Get data from github with the aliases
+ * - Show the columns on the screen with a title
+ * - Show inside the columns the cards
+ *
+ * If you want to add improvements, please create a fork in our GitHub:
+ * https://github.com/myparcelnl
+ *
+ * @author      Richard Perdaan <richard@myparcel.nl>
+ * @author 		Reindert Vetter <reindert@myparcel.nl>
+ * @copyright   2017 MyParcel
+ * @link        https://github.com/myparcelnl/github-projects
+ */
+
+require(['jquery'], function($){
+
+    if(window.mypa == null || window.mypa == undefined){
+        window.mypa = {};
+    }
+    if(window.mypa.fn == null || window.mypa.fn == undefined){
+        window.mypa.fn = {};
+    }
+
+    (function () {
+
+        var getColumns, appendColumns,appendCards, getUrlParameter;
+        window.mypa.load = function () {
+            var columns = getColumns();
+            $.each(columns, function(key, value) {
+                appendColumn(value);
+            });
+        };
+
+        getColumns = function () {
+            return [
+                {
+                    alias: "todo",
+                    title: "Todo"
+                },
+                {
+                    alias: "in-progress",
+                    title: "In progress"
+                },
+                {
+                    alias: "done",
+                    title: "Done"
+                }
+            ];
+        };
+
+        appendColumn = function (column) {
+            $('.mypa_columns').append('<div class="column"><h2 class="column_titel">' + column.title + '</h2><div class="cards" id="label-' + column.alias + '"></div></div>');
+            appendCards(column)
+        };
+
+        appendCards = function (column) {
+            $.ajax({
+                type: 'GET',
+                url: "https://api.github.com/repos/myparcelnl/magento/issues?labels=" + column.alias + "&sort=updated-asc",
+                success : function(issues) {
+                    $.each(issues, function(key, issue) {
+                        $('#label-' + column.alias).append('<div class="card"><a href="' + issue.html_url + '" target="_blank" class="card_url"><h3 class="card_url_color">' + issue.title + '</h3></a></div>');
+                    });
+                }
+            });
+        };
+    })();
+
+    $(document).ready(function() {
+        if ($(".mypa_columns")[0]){
+            window.mypa.load();
+        }
+    });
+
+});
