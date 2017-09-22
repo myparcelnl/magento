0488b6066fdd0a33b453c59d912b6a363446dff0
Richard Perdaan <richardperdaan@live.nl>
2017-09-22 13:19:16 +0200

    Add options-dynamic.min.css

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -6,7 +6,7 @@ var config = {
             'myparcelnl_init_shipping_options': 'MyParcelNL_Magento/js/checkout/shipping_method/show-myparcel-shipping-method',
             'myparcelnl_lib_myparcel': 'MyParcelNL_Magento/js/lib/myparcel',
             'myparcelnl_options_template': 'text!MyParcelNL_Magento/template/checkout/options.html',
-            'myparcelnl_options_css-dynamic': 'text!MyParcelNL_Magento/css/checkout/options-dynamic.css'
+            'myparcelnl_options_css-dynamic': 'text!MyParcelNL_Magento/css/checkout/options-dynamic.min.css'
         }
     }
 };
