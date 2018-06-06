d0a1576855ede092a45478ed3aed8f497dcc72a8
Reindert Vetter <reindert@myparcel.nl>
2018-06-06 10:12:53 +0200

    move requirejs alias

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
     map: {
         '*': {
             'Magento_Tax/js/view/checkout/shipping_method/price': 'MyParcelNL_Magento/js/action/price',
-            "Magento_Checkout/js/model/shipping-save-processor/default" : "MyParcelNL_Magento/js/model/shipping-save-processor-default",
+            "Magento_Checkout/js/model/shipping-save-processor/default" : "MyParcelNL_Magento/js/model/shipping-save-processor-default"
         }
     }
 };
