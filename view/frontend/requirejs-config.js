f6a0e46bd76fdba39648d8657ce4cb7638d703a4
Reindert Vetter <reindert@myparcel.nl>
2018-06-06 11:44:39 +0200

    move requirejs alias

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,3 +1,8 @@
+/**
+ * Override Magento classes
+ *
+ * @type {{map: {"*": {"Magento_Tax/js/view/checkout/shipping_method/price": string, "Magento_Checkout/js/model/shipping-save-processor/default": string}}}}
+ */
 var config = {
     map: {
         '*': {
