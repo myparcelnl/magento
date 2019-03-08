5a9aeeb93fb6e6f91a7138a120cc9dd5eac2038e
Richard Perdaan <richard@myparcel.nl>
2019-03-08 14:54:56 +0100

    get the correct data from the database

--- Plugin/Magento/Sales/Api/Data/OrderExtension.php
+++ Plugin/Magento/Sales/Api/Data/OrderExtension.php
@@ -12,6 +12,12 @@ use Magento\Framework\App\ObjectManager;
 
 class OrderExtension {
 
+    protected $request;
+
+    public function __construct(\Magento\Framework\HTTP\PhpEnvironment\Request $request) {
+       $this->request = $request->setPathInfo();
+    }
+
     /**
      * Avoid default email is sent.
      *
@@ -20,17 +26,23 @@ class OrderExtension {
      * @return string|null
      */
     public function afterGetDeliveryOptions() {
+
         $objectManager =  ObjectManager::getInstance();
 
         $resource = $objectManager->get('Magento\Framework\App\ResourceConnection');
         $connection = $resource->getConnection();
         $tableName = $resource->getTableName('sales_order'); //gives table name with prefix
 
+        $entity_id  = str_replace("/rest/V1/orders/","" ,$this->request->getPathInfo());;
 
         //Select Data from table
-        $sql = "Select `delivery_options` FROM " . $tableName;
+        $sql = $connection
+            ->select('delivery_options')
+            ->from($tableName)
+            ->where('entity_id = '. $entity_id);
+
         $result = $connection->fetchAll($sql); // gives associated array, table fields as key in array.
 
-        return $result;
+        return $result[0]['delivery_options'];
     }
 }
