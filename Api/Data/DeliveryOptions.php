8afca87b0bfc38e824a69343895ab1384dbf4b0f
Richard Perdaan <richard@myparcel.nl>
2019-05-14 13:28:40 +0200

    remove command

--- Api/Data/DeliveryOptions.php
+++ Api/Data/DeliveryOptions.php
@@ -1,24 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: richardperdaan
- * Date: 2019-05-14
- * Time: 13:22
- */
+
 namespace MyParcelNL\Magento\Api\Data;
 interface DeliveryOptions
 {
     const VALUE = 'value';
+
     /**
      * Return value.
      *
      * @return string|null
      */
     public function getValue();
+
     /**
      * Set value.
      *
      * @param string|null $value
+     *
      * @return $this
      */
     public function setValue($value);
