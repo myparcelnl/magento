5b902d776824c5fae0dd802c2a3cbbdd4b3a6a38
Edie Lemoine <edie@myparcel.nl>
2020-03-04 15:20:01 +0100

    fix: display working track & trace link in order overview

--- Ui/Component/Listing/Column/TrackNumber.php
+++ Ui/Component/Listing/Column/TrackNumber.php
@@ -1,59 +1,62 @@
 <?php
-/**
- * Show the status of the track
- *
- * If you want to add improvements, please create a fork in our GitHub:
- * https://github.com/myparcelnl
- *
- * @author      Reindert Vetter <reindert@myparcel.nl>
- * @copyright   2010-2017 MyParcel
- * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US  CC BY-NC-ND 3.0 NL
- * @link        https://github.com/myparcelnl/magento
- * @since       File available since Release v0.1.0
- */
 
 namespace MyParcelNL\Magento\Ui\Component\Listing\Column;
 
-use \Magento\Sales\Api\OrderRepositoryInterface;
-use \Magento\Framework\View\Element\UiComponent\ContextInterface;
-use \Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Sales\Model\Order;
-use \Magento\Ui\Component\Listing\Columns\Column;
-use \Magento\Framework\Api\SearchCriteriaBuilder;
-use MyParcelNL\Sdk\src\Helper\TrackTraceUrl;
+use Magento\Ui\Component\Listing\Columns\Column;
+use MyparcelNL\Sdk\src\Helper\TrackTraceUrl;
 
 class TrackNumber extends Column
 {
     /**
+     * Script tag to unbind the click event from the td wrapping the barcode link.
+     */
+    const SCRIPT_UNBIND_CLICK = "<script type='text/javascript'>jQuery('.myparcel-barcode-link').closest('td').unbind('click');</script>";
+
+    /**
      * Set column MyParcel barcode to order grid
      *
      * @param array $dataSource
-     * @param null $postcode
      *
      * @return array
      */
-    public function prepareDataSource(array $dataSource, $postcode = null)
+    public function prepareDataSource(array $dataSource): array
     {
+        parent::prepareDataSource($dataSource);
+
+        if (! isset($dataSource['data']['items'])) {
+            return $dataSource;
+        }
+
         /**
-         * @var Order $order
+         * @var Order                  $order
          * @var Order\Shipment\Track[] $tracks
          */
-        if (isset($dataSource['data']['items'])) {
-            foreach ($dataSource['data']['items'] as & $item) {
+        foreach ($dataSource['data']['items'] as & $item) {
+            $addressParts = explode(",", $item['shipping_address']);
 
-                $address = explode(",", $item['shipping_address']);
+            if (count($addressParts) === 4) {
+                [$company, $street, $city, $postalCode] = $addressParts;
+            } else {
+                [$street, $city, $postalCode] = $addressParts;
+            }
 
-                /* Get the postcode from address */
-                if ( ! empty($address[2])) {
-                    list($street, $city, $postcode) = explode(",", $item['shipping_address']);
-                }
+            // Stop if either the barcode/status or postal code is missing.
+            if (! $item['track_number'] || ! $postalCode) {
+                continue;
+            }
 
-                if (key_exists('track_number', $item) && $postcode) {
-                    $trackTrace = (new TrackTraceUrl())
-                        ->create($item['track_number'], $postcode, null);
+            $trackNumber = $item['track_number'];
+            $data = $this->getData('name');
 
-                    $item[$this->getData('name')] = '<a target="_blank" href=' . $trackTrace . ' >' . $item['track_number'] . '</a>';
-                }
+            // Only render the T&T as a link and add the script to remove the click handler if it's actually a barcode.
+            if (strpos($trackNumber, '3S') === 0) {
+                // Remove any spaces from the postal code.
+                $trimmedPostalCode  = str_replace(' ', '', $postalCode);
+                $trackTrace  = (new TrackTraceUrl())->create($trackNumber, $trimmedPostalCode);
+
+                $item[$data] = "<a class=\"myparcel-barcode-link\" target=\"_blank\" href=\"$trackTrace\">$trackNumber</a>";
+                $item[$data] .= self::SCRIPT_UNBIND_CLICK;
             }
         }
 
