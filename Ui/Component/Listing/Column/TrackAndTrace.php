d9412f7c24d0e51d27ee5ab3ec764ee9beca5c37
Edie Lemoine <edie@myparcel.nl>
2020-03-20 09:11:41 +0100

    Fix track & trace urls in shipment overview (#370)
    * start with branded track trace
    
    * show track trace link in order grid
    
    * start with tracking pop-up
    
    * replace track.phtml
    
    * start with branded track trace
    
    * show track trace link in order grid
    
    * start with tracking pop-up
    
    * replace track.phtml
    
    * fix: display working track & trace link in order overview
    
    * remove old unfinished code
    
    * Revert: replace track.phtml (93c007fa)
    
    * remove trimming of the postal code because the sdk will handle this
    
    * improve column translations and make the code a little bit nicer
    
    * fix conditional
    
    Co-authored-by: Richard Perdaan <richard@myparcel.nl>

---
+++ Ui/Component/Listing/Column/TrackAndTrace.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace MyParcelNL\Magento\Ui\Component\Listing\Column;
+
+use Magento\Sales\Model\Order;
+use Magento\Ui\Component\Listing\Columns\Column;
+use MyparcelNL\Sdk\src\Helper\TrackTraceUrl;
+
+class TrackAndTrace extends Column
+{
+    const NAME = 'track_number';
+
+    const VALUE_EMPTY = '–';
+
+    /**
+     * Script tag to unbind the click event from the td wrapping the barcode link.
+     */
+    const SCRIPT_UNBIND_CLICK = "<script type='text/javascript'>jQuery('.myparcel-barcode-link').closest('td').unbind('click');</script>";
+
+    /**
+     * Set column MyParcel barcode to order grid
+     *
+     * @param array $dataSource
+     *
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource): array
+    {
+        parent::prepareDataSource($dataSource);
+
+        if (! isset($dataSource['data']['items'])) {
+            return $dataSource;
+        }
+
+        /**
+         * @var Order                  $order
+         * @var Order\Shipment\Track[] $tracks
+         */
+        foreach ($dataSource['data']['items'] as & $item) {
+            $addressParts = explode(",", $item['shipping_address']);
+
+            if (count($addressParts) === 4) {
+                [$company, $street, $city, $postalCode] = $addressParts;
+            } else {
+                [$street, $city, $postalCode] = $addressParts;
+            }
+
+            // Stop if either the barcode or postal code is missing.
+            if (! $item['track_number'] || $item['track_number'] === self::VALUE_EMPTY || ! $postalCode) {
+                continue;
+            }
+
+            $trackNumber = $item['track_number'];
+            $data = $this->getData('name');
+
+            // Render the T&T as a link and add the script to remove the click handler.
+            $trackTrace = (new TrackTraceUrl())->create($trackNumber, $postalCode);
+            $item[$data] = "<a class=\"myparcel-barcode-link\" target=\"_blank\" href=\"$trackTrace\">$trackNumber</a>";
+            $item[$data] .= self::SCRIPT_UNBIND_CLICK;
+        }
+
+        return $dataSource;
+    }
+}
