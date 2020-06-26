325582a3193a3722b48be2f1bce24ee936cb92b1
Richard Perdaan <richard@myparcel.nl>
2020-06-26 15:44:46 +0200

    check if TrackingUrl exsist

--- Block/DataProviders/Email/Shipment/TrackingUrl.php
+++ Block/DataProviders/Email/Shipment/TrackingUrl.php
@@ -3,34 +3,66 @@
 namespace MyParcelNL\Magento\Block\DataProviders\Email\Shipment;
 
 use Magento\Framework\App\ObjectManager;
-use Magento\Sales\Block\DataProviders\Email\Shipment\TrackingUrl as MagentoTrackingUrl;
 use Magento\Sales\Model\Order;
 use Magento\Sales\Model\Order\Shipment\Track;
 use MyParcelNL\Sdk\src\Helper\TrackTraceUrl;
 
-/**
- * Shipment track info for email
- */
-class TrackingUrl extends MagentoTrackingUrl
-{
+// For Magento version < 2.3.2
+if (class_exists('\Magento\Sales\Block\DataProviders\Email\Shipment\TrackingUrl')) {
+
     /**
-     * Get full Track & Trace url for the shipping e-mail
-     *
-     * @param Track $track
-     *
-     * @return string
+     * Shipment track info for email
      */
-    public function getUrl(Track $track): string
+    class TrackingUrl extends \Magento\Sales\Block\DataProviders\Email\Shipment\TrackingUrl
     {
         /**
-         * @var Order
+         * Get full Track & Trace url for the shipping e-mail
+         *
+         * @param Track $track
+         *
+         * @return string
          */
-        $order = (ObjectManager::getInstance())->create(Order::class)->load($track->getOrderId());
+        public function getUrl(Track $track): string
+        {
+            /**
+             * @var Order
+             */
+            $order = (ObjectManager::getInstance())->create(Order::class)->load($track->getOrderId());
 
-        return (new TrackTraceUrl())->create(
-            $track->getNumber(),
-            $order->getShippingAddress()->getPostcode(),
-            $order->getShippingAddress()->getCountryId()
-        );
+            return (new TrackTraceUrl())->create(
+                $track->getNumber(),
+                $order->getShippingAddress()->getPostcode(),
+                $order->getShippingAddress()->getCountryId()
+            );
+        }
+    }
+
+} else {
+
+    /**
+     * Shipment track info for email
+     */
+    class TrackingUrl
+    {
+        /**
+         * Get full Track & Trace url for the shipping e-mail
+         *
+         * @param Track $track
+         *
+         * @return string
+         */
+        public function getUrl(Track $track): string
+        {
+            /**
+             * @var Order
+             */
+            $order = (ObjectManager::getInstance())->create(Order::class)->load($track->getOrderId());
+
+            return (new TrackTraceUrl())->create(
+                $track->getNumber(),
+                $order->getShippingAddress()->getPostcode(),
+                $order->getShippingAddress()->getCountryId()
+            );
+        }
     }
 }
