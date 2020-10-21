998814d374b71ebcd6c09fe68babf5a0e5a7c15e
Richard Perdaan <richard@myparcel.nl>
2020-10-21 09:15:26 +0200

    relplase setUserAgentArray with setUserAgents

--- Model/Sales/MagentoCollection.php
+++ Model/Sales/MagentoCollection.php
@@ -115,7 +115,7 @@ class MagentoCollection implements MagentoCollectionInterface
         $this->helper             = $objectManagerInterface->create(self::PATH_HELPER_DATA);
         $this->modelTrack         = $objectManagerInterface->create(self::PATH_ORDER_TRACK);
         $this->messageManager     = $objectManagerInterface->create(self::PATH_MANAGER_INTERFACE);
-        $this->myParcelCollection = (new MyParcelCollection())->setUserAgentArray(['Magento2'=> $this->helper->getVersion()]);
+        $this->myParcelCollection = (new MyParcelCollection())->setUserAgents(['Magento2'=> $this->helper->getVersion()]);
     }
 
     /**
