c569a07a4b83eeab9c8494ea0fdab33d0fab80ac
Joeri van Veen <nonstockphoto@gmail.com>
2021-07-08 07:55:49 +0200

    fix: handle and show errors during create concepts (#579)

--- Model/Sales/MagentoOrderCollection.php
+++ Model/Sales/MagentoOrderCollection.php
@@ -261,9 +261,22 @@ class MagentoOrderCollection extends MagentoCollection
      * @throws \MyParcelNL\Sdk\src\Exception\MissingFieldException
      * @throws \Exception
      */
-    public function createMyParcelConcepts()
+    public function createMyParcelConcepts(): self
     {
-        $this->myParcelCollection->createConcepts()->setLatestData();
+        if (! count($this->myParcelCollection)) {
+            $this->messageManager->addWarningMessage(__('myparcelnl_magento_error_no_shipments_to_process'));
+            return $this;
+        }
+
+        try {
+            $this->myParcelCollection->createConcepts();
+        } catch (\Exception $e) {
+            $this->messageManager->addErrorMessage($e->getMessage());
+            return $this;
+        }
+
+        $this->myParcelCollection->setLatestData();
+
         /**
          * @var Order                $order
          * @var Order\Shipment       $shipment
