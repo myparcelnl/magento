defca42c348af0d8028e0059dc47f83cff2e6ae5
Richard Perdaan <richard@myparcel.nl>
2021-07-27 10:33:58 +0200

    fix: setting for export mode

---
+++ Model/Source/ExportMode.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Magento\Model\Source;
+
+use \Magento\Framework\Data\OptionSourceInterface;
+
+class ExportMode implements OptionSourceInterface
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray(): array
+    {
+        return [
+            [
+                'value' => 'shipments',
+                'label' => __('Export shipping details only')
+            ],
+            [
+                'value' => 'pps',
+                'label' => __('Export entire order')
+            ]
+        ];
+    }
+
+    /**
+     * Get options in "key-value" format
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'shipments' => __('shipments'),
+            'pps'       => __('pps')
+        ];
+    }
+}
