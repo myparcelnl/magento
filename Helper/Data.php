b38338ff0e2d81c5f05795235ba85afa28e7424a
Richard Perdaan <richard@myparcel.nl>
2021-12-15 13:54:09 +0100

    fix: error delivery date is too early

--- Helper/Data.php
+++ Helper/Data.php
@@ -179,15 +179,15 @@ class Data extends AbstractHelper
             return null;
         }
 
-        $date          = strtotime($date);
-        $delivery_date = date('Y-m-d H:i:s', $date);
-        $todayDate     = strtotime('now');
+        $checkoutData = json_decode($date, true)['date'];
+        $deliveryDate = strtotime(date('Y-m-d', strtotime($checkoutData)));
+        $currentDate  = strtotime(date('Y-m-d'));
 
-        if ($date <= $todayDate) {
+        if ($deliveryDate <= $currentDate) {
             return date('Y-m-d H:i:s', strtotime('now +1 day'));
         }
 
-        return $delivery_date;
+        return $checkoutData;
     }
 
     /**
