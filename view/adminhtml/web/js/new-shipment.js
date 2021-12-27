6310d26d637add5a6af139fcbfcffa3e652fd472
Joeri van Veen <joeri@myparcel.nl>
2021-12-27 13:23:51 +0100

    fix: improvements during testing of scenarios

--- view/adminhtml/web/js/new-shipment.js
+++ view/adminhtml/web/js/new-shipment.js
@@ -16,6 +16,7 @@ define(
             packageTypes = document.querySelectorAll('[name="mypa_package_type"]');
           this.mypa_carrier = options.carrier || 'postnl';
           this.mypa_package_type = options.packageType || 'package';
+          this.leaveAllVisible = false;
           this.initializeSelectors(carriers);
           this.initializeSelectors(packageTypes);
 
@@ -28,7 +29,7 @@ define(
             selector;
           for (i = 0, len = selectors.length; i < len; ++i) {
             selector = selectors[i];
-            selector.addEventListener('change', function() {
+            selector.addEventListener('click', function() {
               self.showForSelector(this);
             });
             if (this.mypa_carrier === selector.value) {
@@ -43,57 +44,55 @@ define(
             timeoutForLoadingSequence = 300,
             i,
             len,
-            element,
-            self = this;
+            element;
+          this[name] = value;
           for (i = 0, len = elements.length; i < len; ++i) {
             element = elements[i];
             if (element.getAttribute('data-for_' + name) === value) {
-              radio = element.querySelector('[type="radio"]');
+              if (element.parentNode.parentNode.hasAttribute('data-for_mypa_carrier')
+                && this.mypa_carrier !== element.parentNode.parentNode.getAttribute('data-for_mypa_carrier')
+              ) {
+                this.toggleElement(element, false);
+                continue;
+              }
               this.toggleElement(element, true);
+              radio = element.querySelector('[value="' + this.mypa_package_type + '"]');
               if (radio) {
                 setTimeout(function(radio) {
-                  self.clickActiveSelector(radio);
+                  radio.click();
                 }, timeoutForLoadingSequence, radio);
               }
-            } else {
-              this.toggleElement(element, false);
+              continue;
             }
-          }
-        },
-        clickActiveSelector: function(radio) {
-          if (radio.value === this.mypa_package_type.toString()) {
-            radio.click();
+            this.toggleElement(element, false);
           }
         },
         toggleElement: function(element, visibility) {
           var inputs = element.getElementsByTagName('input'),
             selects = element.getElementsByTagName('select');
-          element.style.display = visibility ? 'inherit' : 'none';
-          if (visibility) {
-            this.setEnabled(inputs, selects);
-          } else {
-            this.setDisabled(inputs, selects);
-          }
+          element.style.display = visibility || this.leaveAllVisible ? 'inherit' : 'none';
+          this.onEachElement(visibility ? this.setEnabled : this.setDisabled, inputs, selects);
         },
-        // todo the shipment options form elements must be disabled for non-active carriers also directly after loading
-        // todo refactor setEnabled and setDisabled because of duplicate code
-        setEnabled: function() {
-          var type, i, len, elements;
-          for (type in arguments) {
-            elements = arguments[type];
-            for (i = 0, len = elements.length; i < len; ++i) {
-              elements[i].removeAttribute('disabled');
+        onEachElement: function(callback) {
+          var argumentCount,
+            argumentInput,
+            i,
+            len;
+          for (argumentCount in arguments) {
+            argumentInput = arguments[argumentCount];
+            if (callback === argumentInput) {
+              continue;
+            }
+            for (i = 0, len = argumentInput.length; i < len; ++i) {
+              callback(argumentInput[i]);
             }
           }
         },
-        setDisabled: function() {
-          var type, i, len, elements;
-          for (type in arguments) {
-            elements = arguments[type];
-            for (i = 0, len = elements.length; i < len; ++i) {
-              elements[i].setAttribute('disabled', 'disabled');
-            }
-          }
+        setEnabled: function(element) {
+          element.removeAttribute('disabled');
+        },
+        setDisabled: function(element) {
+          element.setAttribute('disabled', 'disabled');
         },
       };
 
