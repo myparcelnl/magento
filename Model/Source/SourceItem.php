02901c8501b500f811ed6535e159d4a12e8f96ca
Richard Perdaan <richard@myparcel.nl>
2022-01-19 17:18:08 +0100

    style: remove unnecessary code

--- Model/Source/SourceItem.php
+++ Model/Source/SourceItem.php
@@ -2,51 +2,19 @@
 
 namespace MyParcelNL\Magento\Model\Source;
 
-use Magento\Framework\Api\SearchCriteriaBuilder;
-use Magento\InventoryApi\Api\Data\SourceItemInterface;
-use Magento\InventoryApi\Api\SourceItemRepositoryInterface;
 use Magento\Sales\Model\Order\ShipmentFactory;
 
 class SourceItem
 {
-    /**
-     * @var SearchCriteriaBuilder
-     */
-    private $searchCriteriaBuilder;
-
-    /**
-     * @var SourceItemRepositoryInterface
-     */
-    private $sourceItemRepository;
-
     /**
      * @var ShipmentFactory
      */
     private $shipmentFactory;
 
     public function __construct(
-        SearchCriteriaBuilder $searchCriteriaBuilder,
-        SourceItemRepositoryInterface $sourceItemRepository,
         ShipmentFactory $shipmentFactory
     ) {
-        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-        $this->sourceItemRepository = $sourceItemRepository;
         $this->shipmentFactory = $shipmentFactory;
-    }
-
-    /**
-     * Retrieves links that are assigned to $stockId
-     *
-     * @param string $sku
-     * @return SourceItemInterface[]
-     */
-    public function getSourceItemDetailBySKU(string $sku): array
-    {
-        $searchCriteria = $this->searchCriteriaBuilder
-            ->addFilter(SourceItemInterface::SKU, $sku)
-            ->create();
-
-        return $this->sourceItemRepository->getList($searchCriteria)->getItems();
     }
 
     /**
@@ -66,8 +34,6 @@ class SourceItem
         );
 
         $extensionAttributes = $shipment->getExtensionAttributes();
-//        var_dump($extensionAttributes->getSourceCode());
-//        exit("\n|-------------\n" . __FILE__ . ':' . __LINE__ . "\n|-------------\n");
         return $extensionAttributes->getSourceCode();
     }
 }
