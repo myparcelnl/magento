5ca40348793b488a3dcc9067a104889e65371745
markernst97 <markiervc@gmail.com>
2022-02-08 14:46:23 +0100

    fix(pps): age check stays false

--- Helper/ShipmentOptions.php
+++ Helper/ShipmentOptions.php
@@ -117,7 +117,7 @@ class ShipmentOptions
         $ageCheckOfProduct    = self::getAgeCheckFromProduct($this->order->getItems());
         $ageCheckFromSettings = self::$defaultOptions->getDefaultOptionsWithoutPrice(self::AGE_CHECK);
 
-        return $ageCheckFromOptions ?? $ageCheckOfProduct ?? $ageCheckFromSettings;
+        return $ageCheckFromOptions ?: $ageCheckOfProduct ?: $ageCheckFromSettings;
     }
 
     /**
