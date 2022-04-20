cf54f15ca4acad8ab41018b4790c4663ccb69759
markernst97 <markiervc@gmail.com>
2022-04-20 17:10:16 +0200

    feat: add custom insurance option

---
+++ Model/Source/InsurancePossibilities.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Magento\Model\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+use MyParcelNL\Sdk\src\Model\Consignment\PostNLConsignment;
+
+class InsurancePossibilities implements OptionSourceInterface
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $array = [];
+
+        foreach ($this->toArray() as $key => $day) {
+            $array[] = [
+                'value' => $key,
+                'label' => $day,
+            ];
+        }
+
+        return $array;
+    }
+
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        $insurancePossibilities = PostNLConsignment::INSURANCE_POSSIBILITIES_LOCAL;
+        $array                  = [];
+
+        foreach ($insurancePossibilities as $i => $iValue) {
+            if ($iValue <= 500) {
+                unset($insurancePossibilities[$i]);
+            }
+        }
+
+        foreach ($insurancePossibilities as $i) {
+            $array[$i] = $i;
+        }
+
+        return $array;
+    }
+}
