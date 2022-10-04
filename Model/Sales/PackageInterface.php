374c37643c2b8f972f8f3bf1383beedb52372c39
Joeri van Veen <joeri@myparcel.nl>
2022-10-04 11:09:11 +0200

    fix: translate instabox strings

--- Model/Sales/PackageInterface.php
+++ Model/Sales/PackageInterface.php
@@ -35,6 +35,11 @@ interface PackageInterface
     public function addWeight(float $weight);
 
     /**
+     * @param bool $all_products_fit
+     */
+    public function setAllProductsFit(bool $all_products_fit);
+
+    /**
      * package = 1
      *
      * @return int
