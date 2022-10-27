00a17c06d9e49aef6a3619548462e9117f11791c
Mark Ernst <33826576+Mark-Ernst@users.noreply.github.com>
2022-10-27 13:04:04 +0200

    fix: remove myparcel product attributes on uninstall (#722)

---
+++ Setup/Uninstall.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Magento\Setup;
+
+use Magento\Eav\Model\ResourceModel\Entity\Attribute;
+use Magento\Eav\Model\ResourceModel\Entity\Attribute\CollectionFactory;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UninstallInterface;
+
+class Uninstall implements UninstallInterface
+{
+    /**
+     * @throws \Exception
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     */
+    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context): void
+    {
+        $setup->startSetup();
+
+        $objectManager        = ObjectManager::getInstance();
+        $eavCollectionFactory = $objectManager->get(CollectionFactory::class);
+        $eavResourceModel     = $objectManager->get(Attribute::class);
+        $eavTypeCollection    = $eavCollectionFactory->create();
+
+        $eavTypeCollection->addFieldToFilter('attribute_code', ['like' => ['myparcel_%']]);
+
+        foreach ($eavTypeCollection as $eavAttribute) {
+            $eavResourceModel->delete($eavAttribute);
+        }
+
+        $setup->endSetup();
+    }
+}
