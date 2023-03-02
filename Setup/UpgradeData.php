31bfcf522c00b63268af8021f7aabe5bcedfbcff
Joeri van Veen <joeri@myparcel.nl>
2023-03-02 11:17:00 +0100

    fix: restore option product not to use mailbox (#757)

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -693,7 +693,13 @@ class UpgradeData implements UpgradeDataInterface
                 Product::ENTITY,
                 'myparcel_fit_in_mailbox',
                 'note',
-                'Fill in the amount of products that fit in a mailbox package. Set to 0 to automatically calculate based on weight. Regardless of this setting, the product will always be sent as a package if the weight is too high.'
+                'Fill in the amount of products that fit in one mailbox package. Use 0 to automatically calculate based on weight, -1 if the article does not fit in a mailbox package. The product will always be sent as a regular package if it\'s too heavy for a mailbox package.'
+            );
+            $eavSetup->updateAttribute(
+                Product::ENTITY,
+                'myparcel_fit_in_mailbox',
+                'default_value',
+                -1
             );
         }
 
