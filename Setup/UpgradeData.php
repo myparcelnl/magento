40ad4633978973bd5506e984b2575e865d3797c6
joerivanveen <joeri@myparcel.nl>
2023-03-14 15:33:00 +0100

    chore: have dutch translations display correctly

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -702,6 +702,14 @@ class UpgradeData implements UpgradeDataInterface
                 -1
             );
         }
+        if (version_compare($context->getVersion(), '4.9.1', '<')) {
+            $eavSetup->updateAttribute(
+                Product::ENTITY,
+                'myparcel_fit_in_mailbox',
+                'note',
+                'Fill in the amount of products that fit in one mailbox package. Use 0 to automatically calculate based on weight, -1 if the article does not fit in a mailbox package. It will always be sent as a regular package if it\'s too heavy for a mailbox package.'
+            );
+        }
 
         $setup->endSetup();
     }
