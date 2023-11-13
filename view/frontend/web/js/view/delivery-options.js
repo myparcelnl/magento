6e33e4f3e7db3a082d0e081bea3a57bfc102039a
joerivanveen <joeri@myparcel.nl>
2023-11-13 10:48:56 +0100

    fix: add dpd pickup price

--- view/frontend/web/js/view/delivery-options.js
+++ view/frontend/web/js/view/delivery-options.js
@@ -88,6 +88,7 @@ define(
         'myparcelnl_magento_dhleuroplus_settings/delivery': 'config.carrierSettings.dhleuroplus.priceStandardDelivery',
         'myparcelnl_magento_dhlparcelconnect_settings/delivery': 'config.carrierSettings.dhlparcelconnect.priceStandardDelivery',
         'myparcelnl_magento_dpd_settings/delivery': 'config.carrierSettings.dpd.priceStandardDelivery',
+        'myparcelnl_magento_dpd_settings/pickup': 'config.carrierSettings.dpd.pricePickup',
       },
 
       /**
