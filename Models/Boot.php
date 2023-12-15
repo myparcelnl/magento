acfdffa65710631ce43bab7aeebc867961f6c720
Thijmen Wijers <thijmen.wijers@myparcel.nl>
2023-12-15 10:13:20 +0100

    wip

---
+++ Models/Boot.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Magento\Models;
+
+use Magento\Framework\App\DeploymentConfig;
+use MyParcelNL\Pdk\Base\Pdk as PdkInstance;
+use MyParcelNL\Magento\Pdk\Hooks\MessageManagerHook;
+use MyParcelNL\Pdk\Account\Platform;
+use MyParcelNL\Pdk\Facade\Installer;
+use MyParcelNL\Pdk\Facade\Pdk;
+use MyParcelNL\Magento\Service\MagentoHookService;
+use function MyParcelNL\Magento\bootPdk;
+use Magento\Framework\App\ObjectManager;
+
+class Boot
+{
+    public function __construct()
+    {
+        var_dump('boot');
+        exit;
+
+        $this->boot();
+
+        /** @var MagentoHookService $hookService */
+        $hookService = Pdk::get(MagentoHookService::class);
+        $hookService->applyAll();
+    }
+
+    private function boot()
+    {
+        // todo: try to find a way to get nl/be from the config to rename name of the plugin
+        $pluginCc = 'nl';
+        var_dump($pluginCc);
+
+        bootPdk(
+            Platform::MYPARCEL_NAME . $pluginCc,
+            'MyParcel',
+            $this->getVersion(),
+            dirname(__FILE__, 3),
+            dirname(__FILE__, 3),
+            PdkInstance::MODE_DEVELOPMENT
+        );
+
+        $myParcelMagentoVersion = sprintf('MYPARCEL%s_MAGENTO_VERSION', strtoupper($pluginCc));
+        if (! defined($myParcelMagentoVersion)) {
+            define($myParcelMagentoVersion, $this->getVersion());
+        }
+
+        $errors = $this->checkPrerequisites();
+
+        if (! empty($errors)) {
+            $this->messageManager->apply([
+                MessageManagerHook::MESSAGE_ERROR,
+                implode('<br>', $errors),
+            ]);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    private function getVersion(): string
+    {
+        $composerVersion = json_decode(file_get_contents(dirname(__FILE__, 3) . '/composer.json'), true);
+
+        return $composerVersion['version'];
+    }
+
+    /**
+     * Check if the minimum requirements are met.
+     *
+     * @return array
+     */
+    private function checkPrerequisites(): array
+    {
+        $errors = [];
+
+        if (! Pdk::get('isPhpVersionSupported')) {
+            $errors[] = Pdk::get('errorMessagePhpVersion');
+        }
+
+        return $errors;
+    }
+}
--- registration.php
+++ registration.php
@@ -17,6 +17,7 @@ License: MIT
 License URI: http://www.opensource.org/licenses/mit-license.php
 */
 
+use Magento\Framework\App\DeploymentConfig;
 use MyParcelNL\Pdk\Base\Pdk as PdkInstance;
 use MyParcelNL\Magento\Pdk\Hooks\MessageManagerHook;
 use MyParcelNL\Pdk\Account\Platform;
@@ -24,132 +25,10 @@ use MyParcelNL\Pdk\Facade\Installer;
 use MyParcelNL\Pdk\Facade\Pdk;
 use MyParcelNL\Magento\Service\MagentoHookService;
 use function MyParcelNL\Magento\bootPdk;
-
-require(__DIR__ . '/vendor/autoload.php');
-
-if (class_exists(MyParcelNLMagento::class)) {
-    throw new RuntimeException('MyParcelNL Magento plugin already loaded');
-}
-
-final class MyParcelNLMagento
-{
-    /**
-     * @var string
-     */
-    private $messageManager;
-
-    public function __construct()
-    {
-        $this->messageManager = Pdk::get(MessageManagerHook::class);
-    }
-
-    /**
-     * Perform required tasks that initialize the plugin.
-     *
-     * @throws \Throwable
-     */
-    public function initialize(): void
-    {
-        $this->boot();
-
-        /** @var MagentoHookService $hookService */
-        $hookService = Pdk::get(MagentoHookService::class);
-        $hookService->applyAll();
-    }
-
-    /**
-     * @return void
-     * @throws \Throwable
-     */
-    public function install(): void
-    {
-        $this->boot();
-
-        $errors = $this->checkPrerequisites();
-
-        if (! empty($errors)) {
-            $this->messageManager->showErrors($errors);
-        }
-
-        Installer::install();
-    }
-
-    /**
-     * @return void
-     * @throws \Exception
-     * @throws \Throwable
-     */
-    public function uninstall(): void
-    {
-        $this->boot();
-
-        Installer::uninstall();
-    }
-
-    /**
-     * @throws \Throwable
-     */
-    private function boot(): void
-    {
-        // todo: try to find a way to get nl/be from the config to rename name of the plugin
-        $pluginCc = 'nl';
-
-        bootPdk(
-            Platform::MYPARCEL_NAME . $pluginCc,
-            'MyParcel',
-            $this->getVersion(),
-            dirname(__FILE__, 3),
-            dirname(__FILE__, 3),
-            PdkInstance::MODE_DEVELOPMENT
-        );
-
-        $myParcelMagentoVersion = sprintf('MYPARCEL%s_MAGENTO_VERSION', strtoupper($pluginCc));
-        if (! defined($myParcelMagentoVersion)) {
-            define($myParcelMagentoVersion, $this->getVersion());
-        }
-
-        $errors = $this->checkPrerequisites();
-
-        if (! empty($errors)) {
-            $this->messageManager->apply([
-                MessageManagerHook::MESSAGE_ERROR,
-                implode('<br>', $errors),
-            ]);
-        }
-    }
-
-    /**
-     * @return string
-     */
-    private function getVersion(): string
-    {
-        $composerVersion = json_decode(file_get_contents(dirname(__FILE__, 3) . '/composer.json'), true);
-
-        return $composerVersion['version'];
-    }
-
-    /**
-     * Check if the minimum requirements are met.
-     *
-     * @return array
-     */
-    private function checkPrerequisites(): array
-    {
-        $errors = [];
-
-        if (! Pdk::get('isPhpVersionSupported')) {
-            $errors[] = Pdk::get('errorMessagePhpVersion');
-        }
-
-        return $errors;
-    }
-}
-
-new MyParcelNLMagento();
+use Magento\Framework\App\ObjectManager;
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    Pdk::getAppInfo()->title,
+    'MyParcelNL_Magento',
     __DIR__
 );
-
