a1895dc21f85ae26b61bed9f55111ce158c477ff
joerivanveen <joeri@myparcel.nl>
2024-01-05 11:16:46 +0100

    fix(orderbeheer): export chosen carrier

--- Model/Sales/MagentoOrderCollection.php
+++ Model/Sales/MagentoOrderCollection.php
@@ -166,7 +166,7 @@ class MagentoOrderCollection extends MagentoCollection
             $defaultOptions          = new DefaultOptions($magentoOrder, $this->helper);
             $myparcelDeliveryOptions = $magentoOrder['myparcel_delivery_options'] ?? '';
             $deliveryOptions         = json_decode($myparcelDeliveryOptions, true);
-            $selectedCarrier         = $this->options['carrier'] ?? CarrierPostNL::NAME;
+            $selectedCarrier         = $deliveryOptions['carrier'] ?? $this->options['carrier'] ?? CarrierPostNL::NAME;
             $shipmentOptionsHelper   = new ShipmentOptions(
                 $defaultOptions,
                 $this->helper,
