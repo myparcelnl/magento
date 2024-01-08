998caaa26d4ccd3862f3d7d9baf20f6d4bcbc181
Thijmen Wijers <thijmen.wijers@myparcel.nl>
2024-01-08 10:26:45 +0100

    wip

--- src/Pdk/Logger/MagentoLogger.php
+++ src/Pdk/Logger/MagentoLogger.php
@@ -8,8 +8,29 @@ use MyParcelNL\Pdk\Logger\AbstractLogger;
 
 class MagentoLogger extends AbstractLogger
 {
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(\Psr\Log\LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param string $level
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     * @throws \Psr\Log\InvalidArgumentException
+     */
     public function log($level, $message, array $context = []): void
     {
-        // TODO: Implement log() method.
+        $this->logger->log($level, $message, $context);
     }
 }
--- src/Pdk/Plugin/Action/MagentoBackendEndpointService.php
+++ src/Pdk/Plugin/Action/MagentoBackendEndpointService.php
@@ -4,12 +4,30 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Magento\src\Pdk\Plugin\Action;
 
+use Magento\Store\Model\StoreManagerInterface;
 use MyParcelNL\Pdk\App\Api\Backend\AbstractPdkBackendEndpointService;
 
 class MagentoBackendEndpointService extends AbstractPdkBackendEndpointService
 {
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        StoreManagerInterface $storeManager
+    ) {
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
     public function getBaseUrl(): string
     {
-        // TODO: Implement getBaseUrl() method.
+        return $this->storeManager->getStore()->getBaseUrl();
     }
 }
