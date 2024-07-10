f8727ee850f7c9dadcd5fcf8f40bf5c06f9a5c06
Joeri van Veen <joeri@myparcel.nl>
2024-07-10 13:13:39 +0200

    fix(checkout): use backwards compatible delivery options (#854)
    INT-564

--- Model/Checkout/ShippingMethods.php
+++ Model/Checkout/ShippingMethods.php
@@ -5,6 +5,7 @@ namespace MyParcelNL\Magento\Model\Checkout;
 use Exception;
 use Magento\Checkout\Model\Session;
 use MyParcelNL\Magento\Api\ShippingMethodsInterface;
+use MyParcelNL\Sdk\src\Factory\DeliveryOptionsAdapterFactory;
 
 /**
  * @since 3.0.0
@@ -23,7 +24,7 @@ class ShippingMethods implements ShippingMethodsInterface
      */
     public function __construct(Session $session)
     {
-        $this->session       = $session;
+        $this->session = $session;
     }
 
     /**
@@ -68,12 +69,13 @@ class ShippingMethods implements ShippingMethodsInterface
     public function persistDeliveryOptions(array $deliveryOptions): array
     {
         $quote = $this->session->getQuote();
-        $quote->addData(['myparcel_delivery_options' => json_encode($deliveryOptions)]);
+        $adapted = DeliveryOptionsAdapterFactory::create($deliveryOptions);
+        $quote->addData(['myparcel_delivery_options' => json_encode($adapted->toArray())]);
         $quote->save();
 
         return [
             'delivery_options' => $deliveryOptions,
-            'message'          => 'Delivery options persisted in quote ' . $quote->getId(),
+            'message' => "Delivery options persisted in quote {$quote->getId()}",
         ];
     }
 }
