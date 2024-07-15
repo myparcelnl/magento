9557aedee85ab66ce0c75ec05ed3b2e54a392c95
Florian de Vries <florian.devries@myparcel.nl>
2024-07-15 10:21:55 +0200

    wip: create migration for insurance

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -854,6 +854,136 @@ class UpgradeData implements UpgradeDataInterface
             }
         }
 
+        if (version_compare($context->getVersion(), '4.15.1', '<=')) {
+            $scope = 'default';
+            $scopeId = 0;
+
+            $carrierPathNames = [
+                'postnl' => 'myparcelnl_magento_postnl_settings/default_options/',
+                'dhlforyou' => 'myparcelnl_magento_dhlforyou_settings/default_options/',
+                'dhleuroplus' => 'myparcelnl_magento_dhleuroplus_settings/default_options/',
+                'dhlparcelconnect' => 'myparcelnl_magento_dhlparcelconnect_settings/default_options/',
+            ];
+            // haal alle oude waardes op
+            // belgie is een aparte omdat hij alleen maar een custom lijkt te hebben.
+            $oldPathNames = [
+                'insurance_belgium_active',
+                'insurance_100_active',
+                'insurance_250_active',
+                'insurance_500_active',
+                'insurance_custom_active',
+                'insurance_belgium_custom_active',
+                'insurance_eu_50_active',
+                'insurance_eu_500_active',
+                'insurance_100_from_price',
+                'insurance_250_from_price',
+                'insurance_500_from_price',
+                'insurance_custom_amount',
+                'insurance_custom_from_price',
+                'insurance_belgium_custom_amount',
+                'insurance_belgium_custom_from_price',
+                'insurance_eu_50_from_price',
+                'insurance_eu_500_from_price',
+            ];
+
+            $newPathNames = [
+                'myparcelnl_magento_dhleuroplus_settings/default_options/'      => 'myparcelnl_magento_dhleuroplus_settings/default_options/insurance_from_price',
+                'myparcelnl_magento_dhleuroplus_settings/default_options/'      => 'myparcelnl_magento_dhleuroplus_settings/default_options/insurance_local_amount',
+                'myparcelnl_magento_dhleuroplus_settings/default_options/'      => 'myparcelnl_magento_dhleuroplus_settings/default_options/insurance_belgium_amount',
+                'myparcelnl_magento_dhleuroplus_settings/default_options/'      => 'myparcelnl_magento_dhleuroplus_settings/default_options/insurance_eu_amount',
+                'myparcelnl_magento_dhleuroplus_settings/default_options/'      => 'myparcelnl_magento_dhleuroplus_settings/default_options/insurance_row_amount',
+                'myparcelnl_magento_dhleuroplus_settings/default_options/'      => 'myparcelnl_magento_dhleuroplus_settings/default_options/insurance_percentage',
+                'myparcelnl_magento_dhlparcelconnect_settings/default_options/' => 'myparcelnl_magento_dhlparcelconnect_settings/default_options/insurance_from_price',
+                'myparcelnl_magento_dhlparcelconnect_settings/default_options/' => 'myparcelnl_magento_dhlparcelconnect_settings/default_options/insurance_eu_amount',
+                'myparcelnl_magento_dhlparcelconnect_settings/default_options/' => 'myparcelnl_magento_dhlparcelconnect_settings/default_options/insurance_row_amount',
+                'myparcelnl_magento_dhlparcelconnect_settings/default_options/' => 'myparcelnl_magento_dhlparcelconnect_settings/default_options/insurance_percentage',
+                'myparcelnl_magento_dhlforyou_settings/default_options/'        => 'myparcelnl_magento_dhlforyou_settings/default_options/insurance_from_price',
+                'myparcelnl_magento_dhlforyou_settings/default_options/'        => 'myparcelnl_magento_dhlforyou_settings/default_options/insurance_local_amount',
+                'myparcelnl_magento_dhlforyou_settings/default_options/'        => 'myparcelnl_magento_dhlforyou_settings/default_options/insurance_belgium_amount',
+                'myparcelnl_magento_dhlforyou_settings/default_options/'        => 'myparcelnl_magento_dhlforyou_settings/default_options/insurance_percentage',
+                'myparcelnl_magento_postnl_settings/default_options/'           => 'myparcelnl_magento_postnl_settings/default_options/insurance_from_price',
+                'myparcelnl_magento_postnl_settings/default_options/'           => 'myparcelnl_magento_postnl_settings/default_options/insurance_local_amount',
+                'myparcelnl_magento_postnl_settings/default_options/'           => 'myparcelnl_magento_postnl_settings/default_options/insurance_belgium_amount',
+                'myparcelnl_magento_postnl_settings/default_options/'           => 'myparcelnl_magento_postnl_settings/default_options/insurance_eu_amount',
+                'myparcelnl_magento_postnl_settings/default_options/'           => 'myparcelnl_magento_postnl_settings/default_options/insurance_row_amount',
+                'myparcelnl_magento_postnl_settings/default_options/'           => 'myparcelnl_magento_postnl_settings/default_options/insurance_percentage',
+            ];
+
+            $newRows = [
+                'postnl' => [],
+                'dhlforyou' => [],
+                'dhleuroplus' => [],
+                'dhlparcelconnect' => [],
+            ];
+
+            $postnl = [
+                'basePath' => 'myparcelnl_magento_postnl_settings/default_options/',
+                'newPaths' => [
+                    'insurance_from_price' => 'myparcelnl_magento_postnl_settings/default_options/insurance_from_price',
+                    'amounts' =>[
+                        'insurance_local_amount' => 'myparcelnl_magento_postnl_settings/default_options/insurance_local_amount',
+                        'insurance_belgium_amount' => 'myparcelnl_magento_postnl_settings/default_options/insurance_belgium_amount',
+                        'insurance_eu_amount' => 'myparcelnl_magento_postnl_settings/default_options/insurance_eu_amount',
+                        'insurance_row_amount' => 'myparcelnl_magento_postnl_settings/default_options/insurance_row_amount',
+                    ],
+                    'insurance_percentage' => 'myparcelnl_magento_postnl_settings/default_options/insurance_percentage',
+                ],
+            ];
+
+            $carriers = [$postnl];
+            $scope = 'default';
+            $scopeId = 0;
+
+            foreach ($carriers as $carrier) {
+                $percentage = 100;
+                $insure_from_price = 0;
+                $carrierPaths = [];
+                $basePath = $carrier['basePath'];
+
+                $updates = [
+                    $basePath . 'insurance_from_price' => $insure_from_price,
+                    $basePath . 'insurance_percentage' => $percentage,
+                ];
+
+                foreach ($oldPathNames as $oldPathName) {
+                    $carrierPaths[] = $carrier['basePath'] . $oldPathName;
+                }
+                $query = $connection->select()->from($table, ['config_id', 'path', 'value'])->where('path IN (?)', $carrierPaths);
+                $queryString = $query->__toString();
+                $rows = $connection->fetchAll($connection->select()->from($table, ['config_id', 'path', 'value'])->where('path IN (?)', $carrierPaths));
+
+                foreach ($carrier['newPaths']['amounts'] as $type => $amountPath) {
+                    if ($type === 'insurance_local_amount') {
+                        $insuranceLocalAmount = 0;
+                        $insurance100Active = $rows[$carrier['basePath'] . 'insurance_100_active'] ?? null;
+                        if ($insurance100Active && $insurance100Active['value'] === '1') {
+                            $insuranceLocalAmount = 99;
+                        }
+                        $updates[$basePath . 'insurance_local_amount'] = $insuranceLocalAmount;
+                    }
+                }
+
+                // ONTHOU DAT JE STEEDS DE VERSIE TERUG MOET ZETTEN!
+                //todo: laat dit stukje hieronder werken. Daarna kun je verder.
+                // Stap 2 voeg de rest van de logica toe
+                foreach ($updates as $path => $value) {
+                    $connection->delete($table, ['path = ?' => $path, 'scope = ?' => $scope, 'scope_id = ?' => $scopeId]);
+                    $connection->insert($table, ['path' => $path, 'value' => $value, 'scope' => $scope, 'scope_id' => $scopeId]);
+                }
+            }
+
+
+
+
+            // maak een array met alle nieuwe waardes. Gebruik hiervoor de oude waardes.
+
+            // voor elke nieuwe waarde kijk je of hij al in de db staat, zo ja, dan update je hem. Zo nee, dan insert je hem.
+
+
+//            $rows = $connection->fetchAll($connection->select()->distinct()->from($table, ['scope', 'scope_id']));
+//            $connection->insert($table, ['path' => 'myparcel/florian-insert-test', 'value' => 'test waarde', 'scope' => 'default', 'scope_id' => 1]);
+        }
+
         $setup->endSetup();
     }
 }
