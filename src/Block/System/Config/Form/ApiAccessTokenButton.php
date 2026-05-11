ac28b6a6c351f5c8eb2def6ea8eab8e7beeb7ac2
Joeri van Veen <joeri@myparcel.nl>
2026-05-11 14:50:52 +0200

    refactor: cleanup

--- src/Block/System/Config/Form/ApiAccessTokenButton.php
+++ src/Block/System/Config/Form/ApiAccessTokenButton.php
@@ -68,7 +68,7 @@ class ApiAccessTokenButton extends Template
         switch ($scope) {
             case ScopeInterface::SCOPE_STORES:
                 return (string) __(
-                    'Covers only this store. Issuing this token removes this store from the default-scope token\'s view and from any parent-website token\'s view, it is now exclusively owned by this store-view-scoped token.'
+                    'Covers only this store. Issuing this token removes this store from the default-scope token\'s view and from any parent-website token\'s view: it is now exclusively owned by this store-view-scoped token.'
                 );
             case ScopeInterface::SCOPE_WEBSITES:
                 return (string) __(
